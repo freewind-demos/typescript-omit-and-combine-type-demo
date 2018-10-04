bac985ed959e1f921756fa091c2aea10b95977fb
freewind <nowindlee@gmail.com>
2018-10-04 20:25:45 +0800

    demo

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-TypeScript Hello World Demo
-============================
+TypeScript Omit and Combine Type Demo
+=====================================
 
-```
-npm install
-npm run demo
-```
-
-It will print `Hello, TypeScript!`
+在typescript中，如何在某个旧类型的基本上，通过重新定义某个属性的类型来得到一个新类型。
--- hello.ts
+++ hello.ts
@@ -1,3 +1,24 @@
-import {capitalize} from 'lodash';
+type User = {
+  id: number,
+  name: string,
+  age: number
+}
 
-console.log("Hello " + capitalize("typescript") + "!");
+type MyExclude<T, U> = T extends U ? never : T;
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+type MyOmit<T, K extends keyof T> = MyPick<T, MyExclude<keyof T, K>>
+type NewUser = MyOmit<User, 'id'> & {id: string}
+
+const user: User = {
+  id: 123,
+  name: 'typescript',
+  age: 3
+};
+
+const newUser: NewUser = {
+  id: 123,
+  name: 'typescript',
+  age: 3
+};
